9a4c8d860ec3a0ef6a2da84f2b5ec745adef4f15
stevenh77 <stevenhollidge@hotmail.com>
2013-11-02 19:50:54 +0100

    animation added

--- FluidDialogWizard/MainPage.xaml.cs
+++ FluidDialogWizard/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
 
 namespace FluidDialogWizard
 {
@@ -9,7 +11,7 @@ namespace FluidDialogWizard
             InitializeComponent();
         }
 
-        private void OpenCloseDialogButton_Click(object sender, RoutedEventArgs e)
+        private void OpenDialogButton_Click(object sender, RoutedEventArgs e)
         {
             // if you want the popup to stretch across the entire width of the screen,
             //  like Win8 then bind PopupContentContainer Width property to {Binding ActualWidth, ElementName=Popup}
@@ -23,7 +25,31 @@ namespace FluidDialogWizard
             if (positioningY > 0)
                 this.Popup.VerticalOffset = positioningY;
 
-            this.Popup.IsOpen = !this.Popup.IsOpen;
+            //this.ContentFrame.Navigate(new Uri("/Views/Page0.xaml", UriKind.Relative));
+            this.Popup.IsOpen = true;
+            VisualStateManager.GoToState(this, "Open", true);
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Popup.IsOpen = false;
+            VisualStateManager.GoToState(this, "Closed", false);
+        }
+
+        private void OneButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //VisualStateManager.GoToState(this, "Open", true);
+            //this.ContentFrame.Navigate(new Uri("/Views/Page1.xaml", UriKind.Relative));
+        }
+
+        private void TwoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //this.ContentFrame.Navigate(new Uri("/Views/Page2.xaml", UriKind.Relative));
+        }
+
+        private void ThreeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //this.ContentFrame.Navigate(new Uri("/Views/Page3.xaml", UriKind.Relative));
         }
     }
 }
---
+++ FluidDialogWizard/Views/Page3.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace FluidDialogWizard.Views
+{
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
