c6a9da65a47b325da72d9abe30837a0ece43fdb2
stevenh77 <stevenhollidge@hotmail.com>
2013-11-02 22:53:32 +0100

    Clip added

---
+++ FluidDialogWizard/Clip.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace FluidDialogWizard
+{
+    public class Clip
+    {
+        public static bool GetToBounds(DependencyObject depObj)
+        {
+            return (bool)depObj.GetValue(ToBoundsProperty);
+        }
+
+        public static void SetToBounds(DependencyObject depObj, bool clipToBounds)
+        {
+            depObj.SetValue(ToBoundsProperty, clipToBounds);
+        }
+
+        public static readonly DependencyProperty ToBoundsProperty =
+            DependencyProperty.RegisterAttached("ToBounds", typeof(bool),
+            typeof(Clip), new PropertyMetadata(false, OnToBoundsPropertyChanged));
+
+        private static void OnToBoundsPropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var fe = d as FrameworkElement;
+            if (fe == null) return;
+            ClipToBounds(fe);
+            fe.Loaded += fe_Loaded;
+            fe.SizeChanged += fe_SizeChanged;
+        }
+
+        private static void ClipToBounds(FrameworkElement fe)
+        {
+            if (GetToBounds(fe))
+            {
+                fe.Clip = new RectangleGeometry()
+                {
+                    Rect = new Rect(0, 0, fe.ActualWidth, fe.ActualHeight)
+                };
+            }
+            else
+            {
+                fe.Clip = null;
+            }
+        }
+
+        static void fe_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ClipToBounds(sender as FrameworkElement);
+        }
+
+        static void fe_Loaded(object sender, RoutedEventArgs e)
+        {
+            ClipToBounds(sender as FrameworkElement);
+        }
+    }
+}
