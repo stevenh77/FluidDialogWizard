87bf7f748db94a14193eb4bfc008fec1dd9a0612
stevenh77 <stevenhollidge@hotmail.com>
2013-11-02 22:40:34 +0100

    removed frame, replaced with modern ui tab control

--- FluidDialogWizard/MainPage.xaml.cs
+++ FluidDialogWizard/MainPage.xaml.cs
@@ -17,39 +17,80 @@ namespace FluidDialogWizard
             //  like Win8 then bind PopupContentContainer Width property to {Binding ActualWidth, ElementName=Popup}
 
             double positioningX = (Application.Current.Host.Content.ActualWidth / 2) - (PopupContentContainer.Width / 2);
-            double positioningY = (Application.Current.Host.Content.ActualHeight / 2) - (PopupContentContainer.Height / 2);
+            double positioningY = 50;
 
             if (positioningX > 0)
                 this.Popup.HorizontalOffset = positioningX;
 
-            if (positioningY > 0)
-                this.Popup.VerticalOffset = positioningY;
+            this.Popup.VerticalOffset = positioningY;
 
-            //this.ContentFrame.Navigate(new Uri("/Views/Page0.xaml", UriKind.Relative));
             this.Popup.IsOpen = true;
-            VisualStateManager.GoToState(this, "Open", true);
         }
 
         private void CloseButton_OnClick(object sender, RoutedEventArgs e)
         {
+            CloseDialog();
+        }
+
+        private void CloseDialog()
+        {
+            this.ModernTabControlUi.SelectedIndex = 0;
             this.Popup.IsOpen = false;
             VisualStateManager.GoToState(this, "Closed", false);
         }
 
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.ModernTabControlUi.SelectedIndex = 1;
+            VisualStateManager.GoToState(this, "Open", true);
+            SetButtons();
+        }
+        
         private void OneButton_OnClick(object sender, RoutedEventArgs e)
         {
-            //VisualStateManager.GoToState(this, "Open", true);
-            //this.ContentFrame.Navigate(new Uri("/Views/Page1.xaml", UriKind.Relative));
+            this.ModernTabControlUi.SelectedIndex = 1;
+            SetButtons();
         }
 
         private void TwoButton_OnClick(object sender, RoutedEventArgs e)
         {
-            //this.ContentFrame.Navigate(new Uri("/Views/Page2.xaml", UriKind.Relative));
+            this.ModernTabControlUi.SelectedIndex = 2;
+            SetButtons();
         }
 
         private void ThreeButton_OnClick(object sender, RoutedEventArgs e)
         {
-            //this.ContentFrame.Navigate(new Uri("/Views/Page3.xaml", UriKind.Relative));
+            this.ModernTabControlUi.SelectedIndex = 3;
+            VisualStateManager.GoToState(this, "Closed", true);
+        }
+
+        private void FinishButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            CloseDialog();
+        }
+        
+        private void SetButtons()
+        {
+            switch (this.ModernTabControlUi.SelectedIndex)
+            {
+                case 1:
+                    this.OneButton.IsEnabled = false;
+                    this.TwoButton.IsEnabled = true;
+                    this.ThreeButton.IsEnabled = true;
+                    break;
+
+                case 2:
+                    this.OneButton.IsEnabled = true;
+                    this.TwoButton.IsEnabled = false;
+                    this.ThreeButton.IsEnabled = true;
+                    break;
+
+                default:
+                    this.OneButton.IsEnabled = true;
+                    this.TwoButton.IsEnabled = true;
+                    this.ThreeButton.IsEnabled = true;
+                    break;
+            }
         }
     }
 }
---
+++ FluidDialogWizard/ModernUITabControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+namespace FluidDialogWizard
+{
+    public class ModernTabControlUi : TabControl
+    {
+        private ContentPresenter content;
+
+        public ModernTabControlUi()
+        {
+            this.SelectionChanged += ModernTabControlUI_SelectionChanged;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            content = GetTemplateChild("ContentTop") as ContentPresenter;
+
+            RunAnimation();
+        }
+
+        private void RunAnimation()
+        {
+            // force content back to having negative offset
+            VisualStateManager.GoToState(this, "Normal", true);
+
+            // slide in content
+            VisualStateManager.GoToState(this, "Transition", true);
+        }
+
+        private void ModernTabControlUI_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.content == null) return;
+
+            RunAnimation();
+        }
+    }
+}
